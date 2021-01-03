db47670dd0e3f16a15bf6caa425cf66c23e85fff
Matthew Kevin Amadeus <13518035@std.stei.itb.ac.id>
2021-01-03 22:39:19 +0700

    ✨ added models for board and tile

--- src/models/Board.ts
+++ src/models/Board.ts
@@ -1,5 +1,12 @@
 import mongoose, { Document, Schema } from "mongoose";
+import { ITile } from "./Tile";
 
-export interface IBoard extends Document{
-  tiles: 
+export interface IBoard extends Document {
+  tile: ITile;
 }
+
+export const BoardSchema = new Schema({
+  tiles: [{ type: Schema.Types.ObjectId, ref: "tile" }],
+});
+
+export default mongoose.model<IBoard>("board", BoardSchema);
--- src/models/Tile.ts
+++ src/models/Tile.ts
@@ -1,7 +1,22 @@
 import mongoose, { Document, Schema } from "mongoose";
 
-export interface ITile extends Document {}
+export enum TileType {
+  START,
+  JAIL,
+  PARKING,
+  PROPERTY,
+  POWER_UP,
+  NONE,
+}
 
-export const TileSchema = new Schema({}, { discriminatorKey: "kind" });
+export interface ITile extends Document {
+  type: TileType;
+  data?: string;
+}
+
+export const TileSchema = new Schema({
+  type: { type: TileType, required: true },
+  data: { type: String },
+});
 
 export default mongoose.model<ITile>("tile", TileSchema);
