70a306a0fd305762a10bebac74532ce0139e3234
Matthew Kevin Amadeus <13518035@std.stei.itb.ac.id>
2021-01-04 23:58:00 +0700

    🌵 Added problem service

---
+++ src/services/ProblemService.ts
@@ -0,0 +1,26 @@
+import Problem, { IProblem } from "@/models/Problem";
+import { Service } from "typedi";
+
+@Service()
+export class ProblemService {
+  public async getAll(): Promise<IProblem[]> {
+    return await Problem.find({});
+  }
+
+  public async getOne(id: string): Promise<IProblem> {
+    return await Problem.findById(id);
+  }
+
+  public async create(data: Partial<IProblem>): Promise<IProblem> {
+    return await new Problem(data).save();
+  }
+
+  public async delete(id: string): Promise<null> {
+    await Problem.findByIdAndDelete(id);
+    return null;
+  }
+
+  public async update(id: string, data: Partial<IProblem>): Promise<IProblem> {
+    return await Problem.findByIdAndUpdate(id, data, { new: true });
+  }
+}
